3cf2c92109be92df8bdfbffc5e79a2c30b9a055a
Rakesh Kumar <rakeshkumar748844@gmail.com>
2023-04-17 09:58:57 -0700

    ver 0.3

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from '../../public/vite.png'
 const Nav = () => {
   return (
     <div className='navMain'>
-        <Link to={'/'} className='imgRoute'><img src='https://static.vecteezy.com/system/resources/previews/004/852/841/original/notes-notepad-notebook-memo-diary-paper-line-icon-illustration-logo-template-suitable-for-many-purposes-free-vector.jpg' alt="logo" className='logo' /></Link>
+        <Link to={'/'} className='imgRoute'><img src={logo} alt="logo" className='logo' /></Link>
         <ul className='navUl'>
             <li>
                 <Link to={'/'}>Home</Link>
